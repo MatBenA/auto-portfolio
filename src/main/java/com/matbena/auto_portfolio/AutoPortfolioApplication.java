7f3676237438415532dc071982c148c6dd3b4932
Matías <crosserain@gmail.com>
2024-08-07 15:14:03 -0300

    initial commit

---
+++ src/main/java/com/matbena/auto_portfolio/AutoPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.matbena.auto_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matbena/auto_portfolio/AutoPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matbena.auto_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
